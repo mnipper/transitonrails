1cca99a99f87d8ca9cf28b0f6c9bdb9df6e13ed9
hodnettg <grayson@logik.com>
2012-11-20 12:42:18 -0500

    add first/last name to users; add wmata key to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120171550) do
+ActiveRecord::Schema.define(:version => 20121120173418) do
 
   create_table "albums", :force => true do |t|
     t.string   "artist"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(:version => 20121120171550) do
     t.datetime "locked_at"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "wmata_key"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true

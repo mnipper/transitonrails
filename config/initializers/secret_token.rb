28c7643c7c7b8b0541ca0a50bebdadf065b5a471
hodnettg <grayson@logik.com>
2012-11-20 11:53:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transitonrails::Application.config.secret_token = '186771a0e82622622e7af427d798b62de1b399db717bea5df71c4872fda3e1649480e3a1d288b0a183c256b47d5562348c0577e19fd29560cc75b4d7389a668b'
